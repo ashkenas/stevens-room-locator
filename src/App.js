07d54bac6d1f8fa7157a5f219fbb75e7c0dc210c
Jacob Ashkenas <jacobashkenas@gmail.com>
2022-09-03 10:54:10 -0400

    Room scraping

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { useState } from 'react';
-import FileUpload from './FileUpload';
-import Schedule from './Schedule';
 
 function App() {
-    const [courses, setCourses] = useState(null);
+    const [data, setData] = useState(null);
 
-    if (!courses)
-        return <FileUpload callback={setCourses} />
-
-    return <Schedule courses={courses} />
+    if (!data) {
+        fetch('http://jacobashkenas.com/cors/?u=https://web.stevens.edu/roomsched/wdprod/', { mode: 'cors' })
+            .then(res => res.text())
+            .then(text => {
+                const doc = new DOMParser().parseFromString(text, 'text/html');
+                const newData = {};
+                newData.rooms = [...doc.querySelectorAll('a > b')].map(el => el.innerText.split('_')[0]);
+                newData.buildings = newData.rooms.map(room => room.split('-')[0]).filter((v, i, a) => a.indexOf(v) === i);
+                setData(newData);
+            });
+    } else {
+        console.log(data);
+    }
 }
 
 export default App;
