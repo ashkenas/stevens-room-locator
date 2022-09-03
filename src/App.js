8b94804590b4ecf3e856e17c24c83e2841c8e055
Jacob Ashkenas <jacobashkenas@gmail.com>
2022-09-03 11:51:03 -0400

    Complete scraping of schedule data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import { useState } from 'react';
 
+const dayLetterToNumber = {
+    M: 0,
+    T: 1,
+    W: 2,
+    R: 3,
+    F: 4,
+    S: 5
+};
+
 function App() {
     const [data, setData] = useState(null);
 
     if (!data) {
-        fetch('http://jacobashkenas.com/cors/?u=https://web.stevens.edu/roomsched/wdprod/', { mode: 'cors' })
+        fetch('https://cors-go-away.herokuapp.com/?u=https://web.stevens.edu/roomsched/wdprod/', { mode: 'cors' })
             .then(res => res.text())
             .then(text => {
                 const doc = new DOMParser().parseFromString(text, 'text/html');
-                const newData = {};
-                newData.rooms = [...doc.querySelectorAll('a > b')].map(el => el.innerText.split('_')[0]);
+                const newData = {
+                    rooms: [],
+                    capacity: {},
+                    schedule: {}
+                };
+
+                const rooms = [...doc.querySelectorAll('a > b')];
+                for (const room of rooms) {
+                    const [roomName, roomCapacity] = room.innerText.split('_');
+                    newData.rooms.push(roomName);
+                    newData.capacity[roomName] = roomCapacity;
+
+                    const schedule = [[], [], [], [], [], []];
+                    [...doc.querySelectorAll(`b[id="${room.innerText}"] + a + table tr`)].slice(1).forEach((row) => {
+                        const dayLetter = row.firstElementChild.innerText;
+                        [...row.querySelectorAll('td[bgcolor]')].forEach(timeBlock => {
+                            const [, course, start, stop] = /^(.*?)\(.*?\[(.*?)-(.*?)\]/.exec(timeBlock.innerText);
+                            schedule[dayLetterToNumber[dayLetter]].push({
+                                course: course,
+                                start: +start.substring(0, 2) + (+start.substring(2) / 60),
+                                stop: +stop.substring(0, 2) + (+stop.substring(2) / 60)
+                            });
+                        });
+                    });
+
+                    newData.schedule[roomName] = schedule;
+                }
+
                 newData.buildings = newData.rooms.map(room => room.split('-')[0]).filter((v, i, a) => a.indexOf(v) === i);
+
                 setData(newData);
             });
     } else {
