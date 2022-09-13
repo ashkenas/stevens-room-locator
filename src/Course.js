353abb74d47321fc1ea962e626a92cae6ae31b6c
Jacob Ashkenas <jacobashkenas@gmail.com>
2022-09-13 14:03:24 -0400

    Course positions now more accurate

--- src/Course.js
+++ src/Course.js
@@ -3,9 +3,11 @@ import "./Course.css";
 function Course({ course, firstHour }) {
     const offset = `${(course.start - firstHour) * 100}%`;
     const height = `${(course.stop - course.start) * 100}%`;
+    const extraOffsetPixels = Math.floor(course.start) - firstHour;
+    const extraHeightPixels = Math.floor(course.stop) - Math.floor(course.start);
 
     return (
-        <div className="course" style={{ top: offset, height: height }}>
+        <div className="course" style={{ top: `calc(${offset} + ${extraOffsetPixels}px)`, height: `calc(${height} + ${extraHeightPixels}px)` }}>
             <p className="course-section">{course.course}</p>
         </div>
     );
