e0e8d96ed5f63ffa7f5904f47697236df2e55fce
Jacob Ashkenas <jacobashkenas@gmail.com>
2023-02-21 16:05:02 -0500

    Height rendering fix

--- src/Course.js
+++ src/Course.js
@@ -4,7 +4,7 @@ function Course({ course, firstHour, setData }) {
     const offset = `${(course.start - firstHour) * 100}%`;
     const height = `${(course.stop - course.start) * 100}%`;
     const extraOffsetPixels = Math.floor(course.start) - firstHour;
-    const extraHeightPixels = Math.floor(course.stop) - Math.floor(course.start);
+    const extraHeightPixels = Math.floor(course.stop - course.start);
 
     return (
         <div className="course" style={{ top: `calc(${offset} + ${extraOffsetPixels}px)`, height: `calc(${height} + ${extraHeightPixels}px)` }}
