1fd23b5615fd3da1c9e7d98b8fa517a212ba4086
Jacob Ashkenas <jacobashkenas@gmail.com>
2024-01-25 13:27:30 -0500

    Added option to search for a course

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import Room from './Room';
 import "./App.css";
 import Spinner from './Spinner';
@@ -26,7 +26,7 @@ function App() {
         setFilters(newFilters);
     };
 
-    if (!data) {
+    useEffect(() => {
         fetch('/api/getSchedule').then(res => res.text()).then(text => {
             const doc = new DOMParser().parseFromString(text, 'text/html');
             const newData = {
@@ -49,6 +49,7 @@ function App() {
                         if (dayLetterToNumber[dayLetter] === undefined) return;
                         schedule[dayLetterToNumber[dayLetter]].push({
                             course: course,
+                            courseSearch: course.replaceAll(/[ \-]/g, '').toLowerCase(),
                             prof: prof,
                             students: students,
                             classCap: classCap,
@@ -64,20 +65,13 @@ function App() {
             }
         
             newData.buildings = newData.rooms.map(room => room.split('-')[0]).filter((v, i, a) => a.indexOf(v) === i);
-            newData.maxCap = Math.max(...Object.values(newData.capacity));
-            newData.minCap = Math.min(...Object.values(newData.capacity));
 
             setData(newData);
-            setFilters({ ...filters, capacity: newData.minCap });
         });
+    }, []);
 
-        return <Spinner />;
-    } else {
-        if (filters.capacity && filters.capacity > data.maxCap)
-            setFilters({ ...filters, capacity: data.maxCap });
-        if (filters.capacity && filters.capacity < data.minCap)
-            setFilters({ ...filters, capacity: data.minCap });
-
+    const validRooms = useMemo(() => {
+        if (!data) return;
         let validRooms = Object.keys(data.capacity);
         if (filters.building)
             validRooms = validRooms.filter(room => room.startsWith(`${filters.building}-`));
@@ -102,44 +96,70 @@ function App() {
                 return false;
             });
         }
+        if (filters.course) {
+            const searchTerm = filters.course.replaceAll(/[ \-]/g, '').toLowerCase();
+            validRooms = validRooms.filter(room => {
+                for (const day in data.schedule[room])
+                    for (const { courseSearch } of data.schedule[room][day])
+                        if (courseSearch.includes(searchTerm))
+                            return true;
+                return false;
+            });
+        }
+        return validRooms;
+    }, [data, filters]);
 
-        return (<>
-            <div className="controls">
-                <div className="field">
-                    <button disabled>Building</button>
-                    <select onChange={setFilter.bind(null, 'building')}>
-                        <option value="undefined">Any Building</option>
-                        {data.buildings.map(building => <option key={building} value={building}>{building}</option>)}
-                    </select>
-                </div>
-                <div className="field">
-                    <button disabled>Min Capacity</button>
-                    <input onChange={setFilter.bind(null, 'capacity')} type="number" min={1} max={999} step={1} value={filters.capacity} />
-                </div>
-                <div className="field">
-                    <button disabled>Day</button>
-                    <select onChange={setFilter.bind(null, 'day')}>
-                        <option value="0">Mon</option>
-                        <option value="1">Tue</option>
-                        <option value="2">Wed</option>
-                        <option value="3">Thu</option>
-                        <option value="4">Fri</option>
-                        <option value="5">Sat</option>
-                    </select>
-                </div>
-                <div className="field">
-                    <button disabled>Start Time</button>
-                    <input onChange={setFilter.bind(null, 'start')} type="time" />
-                </div>
-                <div className="field">
-                    <button disabled>End Time</button>
-                    <input onChange={setFilter.bind(null, 'stop')} type="time" />
-                </div>
+    if (!data) return <Spinner />;
+    console.log(data);
+
+    return (<>
+        <h1>SIT Room Locator</h1>
+        <p>Locate empty rooms on campus for any given time, or just check a specific room's schedule.</p>
+        <div className="controls">
+            <div className="field">
+                <button disabled>Building</button>
+                <select onChange={setFilter.bind(null, 'building')}>
+                    <option value="undefined">Any Building</option>
+                    {data.buildings.map(building => <option key={building} value={building}>{building}</option>)}
+                </select>
             </div>
-            {validRooms.map(room => <Room key={room} room={room} capacity={data.capacity[room]} schedule={data.schedule[room]} setData={setPopup} /> )}
-            <Popup data={popup} />
-        </>);
-    }
+            <div className="field">
+                <button disabled>Min Capacity</button>
+                <input onChange={setFilter.bind(null, 'capacity')} type="number" min={1} max={999} step={1} value={filters.capacity} />
+            </div>
+            <div className="field">
+                <button disabled>Day</button>
+                <select onChange={setFilter.bind(null, 'day')}>
+                    <option value="0">Mon</option>
+                    <option value="1">Tue</option>
+                    <option value="2">Wed</option>
+                    <option value="3">Thu</option>
+                    <option value="4">Fri</option>
+                    <option value="5">Sat</option>
+                </select>
+            </div>
+            <div className="field">
+                <button disabled>Start Time</button>
+                <input onChange={setFilter.bind(null, 'start')} type="time" />
+            </div>
+            <div className="field">
+                <button disabled>End Time</button>
+                <input onChange={setFilter.bind(null, 'stop')} type="time" />
+            </div>
+            <div className="field">
+                <button disabled>Find Course</button>
+                <input onChange={setFilter.bind(null, 'course')} type="text" />
+            </div>
+        </div>
+        {validRooms.map(room =>
+            <Room key={room}
+                room={room}
+                capacity={data.capacity[room]}
+                schedule={data.schedule[room]}
+                setData={setPopup} />
+        )}
+        <Popup data={popup} />
+    </>);
 }
 
 export default App;
